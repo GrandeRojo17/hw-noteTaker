0ec8ac5cf82f57ba6760536349f01ee25ed70caf
granderojo17 <david.yennerell@gmail.com>
2020-05-30 21:38:53 -0500

    update 2

--- server.js
+++ server.js
@@ -2,3 +2,21 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3369;
+
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3369;
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+//Set up routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+// Start up the server.
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
